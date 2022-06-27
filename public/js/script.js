e17d945ebe1c96d094372b810ccc0fc698312a6c
winny314 <winny3142019@gmail.com>
2022-06-28 01:34:02 +0800

    0.2.4(2022.06.28)footer的icon修改2

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,8 @@ document.getElementById("btn-nav-toggle").addEventListener('click',openNav)
             if( document.getElementById('primary-navigation').classList.contains('active') ==true ){
                 // 假如主導覽有active類別時執行
                 document.getElementById('primary-navigation').classList.remove('active')
-                document.getElementById('btn-nav-toggle').textContent = 'Menu'
+                document.getElementById('btn-nav-toggle').textContent = 'Menu' 
+                
             }else{
                 // 假如主導覽沒有active類別時執行
                 document.getElementById('primary-navigation').classList.add('active')
